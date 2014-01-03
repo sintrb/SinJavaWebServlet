56cf434cb0fdb7a61020cef77cdf253a28f05c9b
RobinTang <sintrb@gmail.com>
2014-01-03 16:52:21 +0800

    Add Servlet source code.

---
+++ src/com/sin/java/web/server/servlet/ServletContainer.java
@@ -0,0 +1,16 @@
+package com.sin.java.web.server.servlet;
+
+import com.sin.java.web.server.BaseHandler;
+import com.sin.java.web.server.exception.WebException401Unauthorized;
+import com.sin.java.web.server.servlet.test.TestServlet;
+
+public class ServletContainer extends BaseHandler{
+	public String handle() throws WebException401Unauthorized{
+		
+		
+		TestServlet ts = new TestServlet();
+//		return getRequestHeader().getPath();
+		throw new WebException401Unauthorized(this, this.webServer);
+//		return "";
+	}
+}
---
+++ src/com/sin/java/web/server/servlet/test/TestServlet.java
@@ -0,0 +1,18 @@
+package com.sin.java.web.server.servlet.test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class TestServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().write("Hello World~~~");
+	}
+}
